945bd76cd3f2b16a791330b10de9217dd7f8eb5a
Reza <srezarafiee7900@gmail.com>
2022-03-09 13:19:10 +0330

    Add Key to Generate

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -4,6 +4,15 @@ print("\nCreated by \"Reza Rafiee\".08-Mar-22")
 Win = Tur.Screen()
 Win.title("Password Generator \U0001F510")
 Win.bgcolor("black")
+Txt = Tur.Turtle()
+Txt.shapesize(0.01)
+Txt.goto(0, 250)
+Txt.pencolor("white")
+Txt.write("Press \"Space\" to Generate", font=(
+    "calibri", 20, "italic"), align="center")
+Txt.hideturtle
+Tur.Turtle()
+Tur.shapesize(0.01)
 Tur.pencolor("white")
 Upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 Lower = "abcdefghijklmnopqrstuvwxyz"
@@ -13,8 +22,15 @@ All = Upper + Lower + Number + Symbol
 Lenght = random.randint(8, 12)
 Password = "".join(random.sample(All, Lenght))
 Style = ('Arial', 20, 'bold')
-Tur.write(
-    f"The generated password is  :  {Password}",
-    font=Style, align="center")
+
+
+def Generate():
+    Tur.write(
+        f"The generated password is  :  {Password}",
+        font=Style, align="center")
+
+
+Win.onkey(Generate, "space")
+Win.listen()
 Tur.hideturtle()
 Win.mainloop()
